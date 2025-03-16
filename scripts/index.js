8ba398f8dcbafc826b842cd8f9fa02f14a37ec94
desperhell <volckov.prohor@yandex.ru>
2025-03-16 22:05:23 +0700

    сделал удаление и появление карточек

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,33 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const content = document.querySelector('.content');
+const placesList = content.querySelector('.places__list');
+
+function createCard(initialcard, deleteCard) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  
+  for (let i = 0; i < initialcard.length; i++) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+    cardElement.querySelector('.card__image').setAttribute('src', initialcard[i].link); 
+    cardElement.querySelector('.card__title').textContent = initialcard[i].name;
+
+    cardElement.querySelector('.card__delete-button').addEventListener('click', function(evt) {
+      deleteCard(evt);
+    });
+  
+    placesList.append(cardElement);
+  }
+  
+}
+
+const deleteCard = (evt) => {
+  const card = evt.target.closest('.card');
+  if (card) {
+    card.remove();
+  }
+};
+
+createCard(initialCards, deleteCard);
